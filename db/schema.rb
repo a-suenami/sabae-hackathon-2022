45d860e19765f2e0766f286622e450f818808ccd
Akira Suenami <a.suenami@gmail.com>
2022-12-18 04:27:04 +0900

    Scaffold questions.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_17_150342) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_17_191324) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.json "photo"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "samples", force: :cascade do |t|
     t.string "name", null: false
