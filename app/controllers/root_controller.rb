3177f649917a309e5c7dfaf4ddc0fe799c44c04f
Akira Suenami <a.suenami@gmail.com>
2022-12-18 04:09:33 +0900

    Use slim.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,6 +1,5 @@
 class RootController < ActionController::Base
   def index
-    sample = Sample.create!(name: 'world')
-    render plain: "hello, #{sample.name}!"
+    @sample = Sample.create!(name: 'world')
   end
 end
