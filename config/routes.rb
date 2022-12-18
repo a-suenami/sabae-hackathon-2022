a5c3e166baf49aef06aadbc1422fc1a1e0cf6521
Akira Suenami <a.suenami@gmail.com>
2022-12-18 11:36:56 +0900

    Fix bug.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "root#index"
 
-  resources :questions, only: [:index, :show]
+  resources :questions, only: [:index, :new, :create]
 
   namespace :admin do
     resources :inquiries, only: [:index, :edit]
