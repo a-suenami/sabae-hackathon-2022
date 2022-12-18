c417ef0d665bbef4d9a4762b7c269bb0ad1e4d9f
Akira Suenami <a.suenami@gmail.com>
2022-12-18 10:17:31 +0900

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :questions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "root#index"
+
+  resources :questions
+
+  get '/sample', to: 'root#sample'
 end
